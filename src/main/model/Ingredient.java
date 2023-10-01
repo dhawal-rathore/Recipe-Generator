4cb91f750e65aefe759db96036f4731c5f84d5f8
Dhawal Rathore <dhawalrathor12@gmail.com>
2023-10-01 01:08:52 -0700

    Added functions

--- src/main/model/Ingredient.java
+++ src/main/model/Ingredient.java
@@ -3,9 +3,13 @@ package model;
 // Represents an Ingredient with name and quantity with optional description
 public class Ingredient {
 
+    public Ingredient(String name, double quantity) {
+        //stub
+    }
+
     //REQUIRES: Ingredient to have same name
-    //EFFECTS: returns True if Ingredient has quantity >= to Ingredient provided
-    public Boolean hasQuantityMoreThanRequired(Ingredient that) {
+    //EFFECTS: returns True if Ingredient has quantity >= to Ingredient that provided
+    public Boolean hasQuantityMoreThan(Ingredient that) {
         return false;
     }
 
@@ -21,4 +25,18 @@ public class Ingredient {
         //stub
     }
 
+    //REQUIRES: 0 < addQuantity
+    //MODIFIES: this
+    //EFFECTS: increases quantity by addQuantity
+    public void addIngredient(double addQuantity) {
+        //stub
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public double getQuantity() {
+        return 0.0;
+    }
 }
