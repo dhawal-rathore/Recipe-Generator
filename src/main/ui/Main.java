76650ac172d03d6a6b15929ff03b6c1c508f4050
Dhawal Rathore <dhawalrathor12@gmail.com>
2023-10-11 14:55:09 -0700

    added functionality to run the program

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,7 +1,11 @@
 package ui;
 
+//main runnable file of the program
 public class Main {
     public static void main(String[] args) {
-
+        RecipeGeneratorConsole console = new RecipeGeneratorConsole();
+        while (console.getShouldProgramContinueRunning()) {
+            console.mainInterface();
+        }
     }
 }
