7a457723eb6b8e42e35ef82fc4257dac1c9a5cdc
Dhawal Rathore <dhawalrathor12@gmail.com>
2023-11-12 18:49:49 -0800

    moved to console

--- src/main/ui/console/ConsoleMain.java
+++ src/main/ui/console/ConsoleMain.java
@@ -1,7 +1,7 @@
-package ui;
+package ui.console;
 
 //main runnable file of the program
-public class Main {
+public class ConsoleMain {
     public static void main(String[] args) {
         RecipeGeneratorConsole console = new RecipeGeneratorConsole();
         while (console.getShouldProgramContinueRunning()) {
