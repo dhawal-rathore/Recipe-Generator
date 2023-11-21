be0f576d167c5a7a7d87687ed564b5ac65f01b07
Dhawal Rathore <dhawalrathor12@gmail.com>
2023-11-20 17:37:22 -0800

    added save and load file dialog boxes

--- src/main/ui/gui/MainFrame.java
+++ src/main/ui/gui/MainFrame.java
@@ -3,19 +3,80 @@ package ui.gui;
 import model.Ingredient;
 import model.Recipe;
 import model.RecipeGenerator;
+import persistence.JsonReader;
+import persistence.JsonWriter;
 import ui.gui.panels.MainPanel;
-import ui.gui.panels.RecipePanel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class MainFrame extends JFrame {
     public static final int HEIGHT = 300 * 3;
     public static final int WIDTH = 400 * 3;
     public static final Font FONT = new Font("Helvetica", Font.PLAIN, 44);
+    private static final String FILE_LOCATION = "./data/RecipeGenerator.json";
+
+
+    private RecipeGenerator generator;
 
     public MainFrame() {
         super("Main Window");
+        frameSetup();
+        askUserToLoad();
+        askUserToSave();
+//        recipeGeneratorSetup(generator);
+        changePanel(new MainPanel(generator,this));
+    }
+
+    public void changePanel(JPanel panel) {
+        getContentPane().removeAll();
+        getContentPane().add(panel, BorderLayout.CENTER);
+        getContentPane().doLayout();
+        update(getGraphics());
+        pack();
+        setVisible(true);
+    }
+
+    private void askUserToLoad() {
+        int n = JOptionPane.showConfirmDialog(
+                this,
+                "Would you like to load saved file?",
+                "Load File?",
+                JOptionPane.YES_NO_OPTION);
+        if (n == 0) {
+            loadGenerator();
+        } else {
+            generator = new RecipeGenerator();
+        }
+    }
+
+    private void loadGenerator() {
+        try {
+            JsonReader reader = new JsonReader(FILE_LOCATION);
+            generator = reader.read();
+            System.out.println("Read from file successful.");
+        } catch (IOException e) {
+            System.out.println("Please save to file first and try again.");
+        }
+    }
+
+    private void saveGenerator() {
+        try {
+            JsonWriter writer = new JsonWriter(FILE_LOCATION);
+            writer.open();
+            writer.write(generator);
+            writer.close();
+            System.out.println("Successfully written to file.");
+        } catch (FileNotFoundException e) {
+            System.err.println("File cannot be written");
+        }
+    }
+
+    private void frameSetup() {
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (Exception e) {
@@ -26,20 +87,22 @@ public class MainFrame extends JFrame {
         setPreferredSize(new Dimension(WIDTH,HEIGHT));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setUndecorated(false);
-
-        RecipeGenerator generator = new RecipeGenerator();
-        recipeGeneratorSetup(generator);
-        changePanel(new MainPanel(generator,this));
-
     }
 
-    public void changePanel(JPanel panel) {
-        getContentPane().removeAll();
-        getContentPane().add(panel, BorderLayout.CENTER);
-        getContentPane().doLayout();
-        update(getGraphics());
-        pack();
-        setVisible(true);
+    private void askUserToSave() {
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent we) {
+                int result = JOptionPane.showConfirmDialog(MainFrame.this,
+                        "Do you want to save to file ?", "Save Confirmation ",
+                        JOptionPane.YES_NO_OPTION);
+                if (result == JOptionPane.YES_OPTION) {
+                    saveGenerator();
+                    MainFrame.this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                } else if (result == JOptionPane.NO_OPTION) {
+                    MainFrame.this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                }
+            }
+        });
     }
 
     @SuppressWarnings({"checkstyle:MethodLength", "checkstyle:SuppressWarnings"})
