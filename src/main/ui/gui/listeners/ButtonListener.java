56ee93d2b47f3cd8d0a8b49036c07158b251e467
Dhawal Rathore <dhawalrathor12@gmail.com>
2023-11-24 02:49:17 -0800

    added documentation

--- src/main/ui/gui/listeners/ButtonListener.java
+++ src/main/ui/gui/listeners/ButtonListener.java
@@ -10,6 +10,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
 
+//implements actionListener for all classes
 public class ButtonListener implements ActionListener {
     private MainFrame originalFrame;
 
@@ -17,7 +18,8 @@ public class ButtonListener implements ActionListener {
         this.originalFrame = originalFrame;
     }
 
-
+    //MODIFIES: originalFrame
+    //EFFECTS: reads actionCommand and runs appropriate method
     @SuppressWarnings("checkstyle:MethodLength")
     public void actionPerformed(ActionEvent e) {
         String actionCommand = ((JButton)e.getSource()).getActionCommand();
@@ -44,29 +46,36 @@ public class ButtonListener implements ActionListener {
                 changeToNextPanel();
                 break;
             default:
-                System.out.println("ok");
-                System.out.println(actionCommand);
+                System.err.println("unrecognized button");
                 break;
         }
 
     }
 
+    //MODIFIES: originalFrame
+    //EFFECTS: switches panel to an ingredient panel specified in button
     private void changeToIngredientPanel(ActionEvent e) {
         Ingredient ingredient = (Ingredient) ((JButton)e.getSource()).getClientProperty("ingredient");
         originalFrame.switchPanelTo(new IngredientPanel(ingredient));
     }
 
+    //MODIFIES: originalFrame
+    //EFFECTS: switches panel to an ingredient list panel specified in button
     private void changeToIngredientListPanel(ActionEvent e) {
         List<Ingredient> ingredientList;
         ingredientList = (List<Ingredient>) ((JButton)e.getSource()).getClientProperty("ingredientList");
         originalFrame.switchPanelTo(new IngredientListPanel(ingredientList,originalFrame));
     }
 
+    //MODIFIES: originalFrame
+    //EFFECTS: switches panel to an recipe panel specified in button
     private void changeToRecipePanel(ActionEvent e) {
         Recipe recipe = (Recipe) ((JButton)e.getSource()).getClientProperty("recipe");
         originalFrame.switchPanelTo(new RecipePanel(recipe));
     }
 
+    //MODIFIES: originalFrame
+    //EFFECTS: switches panel to an recipe list panel specified in button
     private void changeToRecipeListPanel(ActionEvent e) {
         List<Recipe> recipeList;
         recipeList = (List<Recipe>) ((JButton)e.getSource()).getClientProperty("recipeList");
@@ -74,14 +83,20 @@ public class ButtonListener implements ActionListener {
         originalFrame.switchPanelTo(new RecipeListPanel(recipeList,originalFrame));
     }
 
+    //MODIFIES: originalFrame
+    //EFFECTS: switches panel to add ingredient to available ingredients panel specified in button
     private void changeToAddIngredientPanel() {
         originalFrame.switchPanelTo(new IngredientInputPanel(originalFrame));
     }
 
+    //MODIFIES: originalFrame
+    //EFFECTS: switches panel to previous panel
     private void changeToPreviousPanel() {
         originalFrame.previousPanel();
     }
 
+    //MODIFIES: originalFrame
+    //EFFECTS: switches panel to next panel if it exists
     private void changeToNextPanel() {
         originalFrame.nextPanel();
     }
